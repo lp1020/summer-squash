9bf8f4e8ae85e2f6d3cd5930379af669abe1060c
Liana Penn <lianapenn@gmail.com>
2020-04-22 21:29:49 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+  var s = skrollr.init();
+   $(window).scroll(function(){
+        $(".fade-out").css("opacity", 1 - $(window).scrollTop() / ($('.fade-out').height() - 500));
+    });
+})
+
+
+
+
